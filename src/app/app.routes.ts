47f7cbd6fde9559d4a99236173064d49b329326d
guilherme <guivillarinho15@gmail.com>
2025-01-06 20:34:10 -0300

    Add Tailwind CSS configuration and update styles; enhance Pokemon component layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
 
 export const routes: Routes = [
+  {path: '', component: PokemonComponent},
   {path: 'pokemons', component: PokemonComponent}
 ];
